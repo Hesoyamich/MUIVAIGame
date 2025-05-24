e9a77dd6e89ac3f9941e39819887c5a7c5f9b4b3
artemmelnikov <lolworm952@gmail.com>
2025-05-24 21:57:50 +0300

    Добавление метода обучения

--- dqnagent.py
+++ dqnagent.py
@@ -21,6 +21,13 @@ class DQNAgent:
         self.target_model = self.build_model()
         self.update_target_network()
 
+    def take_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.randint(self.action_size)  # Исследование
+        state_tensor = tf.convert_to_tensor([state])
+        q_values = self.model(state_tensor)
+        return tf.argmax(q_values[0]).numpy()
+
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
@@ -33,5 +40,32 @@ class DQNAgent:
         model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=0.001))
         return model
     
+    def train(self):
+        if len(self.memory) < self.batch_size:
+            return
+        
+        # Выборка батча из памяти
+        minibatch = np.random.choice(len(self.memory), self.batch_size, replace=False)
+        states = np.array([self.memory[i][0] for i in minibatch])
+        actions = np.array([self.memory[i][1] for i in minibatch])
+        rewards = np.array([self.memory[i][2] for i in minibatch])
+        next_states = np.array([self.memory[i][3] for i in minibatch])
+        dones = np.array([self.memory[i][4] for i in minibatch])
+
+        # Подсчёт Q-чисел
+        target_q = self.model.predict(states)
+        target_next_q = self.target_model.predict(next_states)
+        
+        for i in range(self.batch_size):
+            if dones[i]:
+                target_q[i][actions[i]] = rewards[i]
+            else:
+                target_q[i][actions[i]] = rewards[i] + self.gamma * np.max(target_next_q[i])
+
+        self.model.train_on_batch(states, target_q)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
     def update_target_network(self):
         self.target_model.set_weights(self.model.get_weights())
