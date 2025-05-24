a699a928296b7baaf6eaee2d6afff6e09ae4b484
artemmelnikov <lolworm952@gmail.com>
2025-05-24 20:13:00 +0300

    Реализация метода reset

--- scripts/TaxiDriver.py
+++ scripts/TaxiDriver.py
@@ -8,34 +8,31 @@ class TaxiDriver:
         self.player_pos = None
         self.passenger_pos = None
         self.destination_pos = None
+        self.pits = []
+        self.obstacles = []
         self.has_passenger = False
         self.done = False
 
     def reset(self):
-        self.agent_pos = (np.random.randint(self.size), np.random.randint(self.size))
-        self.passenger_pos = (np.random.randint(self.size), np.random.randint(self.size))
-        while self.passenger_pos == self.agent_pos:
-            self.passenger_pos = (np.random.randint(self.size), np.random.randint(self.size))
-        self.destination_pos = (np.random.randint(self.size), np.random.randint(self.size))
-        while self.destination_pos == self.agent_pos or self.destination_pos == self.passenger_pos:
-            self.destination_pos = (np.random.randint(self.size), np.random.randint(self.size))
-        self.has_passenger = False
-        self.done = False
-
-        self.traps = []
+        # Случайные координаты объектов на карте.
+        self.agent_pos = [np.random.randint(self.size), np.random.randint(self.size)]
+        self.passenger_pos = [np.random.randint(self.size), np.random.randint(self.size)]
+        while self.passenger_pos == self.player_pos:
+            self.passenger_pos = [np.random.randint(self.size), np.random.randint(self.size)]
+        self.destination_pos = [np.random.randint(self.size), np.random.randint(self.size)]
+        while self.destination_pos in (self.passenger_pos, self.player_pos):
+            self.destination_pos = [np.random.randint(self.size), np.random.randint(self.size)]
+        
+        self.pits = []
         self.obstacles = []
-        for _ in range(3):  
-            pos = (np.random.randint(self.size), np.random.randint(self.size))
-            while pos in [self.agent_pos, self.passenger_pos, self.destination_pos] or pos in self.traps:
-                pos = (np.random.randint(self.size), np.random.randint(self.size))
-            self.traps.append(pos)
-        for _ in range(5): 
-            pos = (np.random.randint(self.size), np.random.randint(self.size))
-            while pos in [self.agent_pos, self.passenger_pos, self.destination_pos] or pos in self.traps + self.obstacles:
-                pos = (np.random.randint(self.size), np.random.randint(self.size))
+        for i in range(3):
+            pos = [np.random.randint(self.size), np.random.randint(self.size)]
+            while pos in [self.agent_pos, self.passenger_pos, self.destination_pos] or pos in self.pits:
+                pos = [np.random.randint(self.size), np.random.randint(self.size)]
+            self.pits.append(pos)
+        for i in range(5):
+            pos = [np.random.randint(self.size), np.random.randint(self.size)]
+            while pos in [self.agent_pos, self.passenger_pos, self.destination_pos] or pos in self.pits + self.obstacles:
+                pos = [np.random.randint(self.size), np.random.randint(self.size)]
             self.obstacles.append(pos)
 
-        self.has_passenger = False
-        self.done = False
-        return self._get_state()
-        return self._get_state()
